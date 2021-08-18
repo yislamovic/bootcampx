27de21d693d0a85dd620aca16bba62700e3b2641
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-08-18 16:22:49 -0400

    added node-postgress which allows me to run SQL withinjavascript code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,14 +10,16 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${args[2]}%'
-LIMIT ${args[3]};
+ORDER BY teacher
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(user.cohort + ': ' + user.teacher);
   })
 }).catch(err => console.error('query error', err.stack));
